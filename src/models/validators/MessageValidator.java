61de66322bc23e01037284afb97760eb2c987e09
yusuke.suzuki <mrs.aoringo@gmail.com>
2020-06-16 17:58:29 +0900

    Add validation

---
+++ src/models/validators/MessageValidator.java
@@ -0,0 +1,30 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Message;
+
+public class MessageValidator {
+ // バリデーションを実行する
+    public static List<String> validate(Message m) {
+        List<String> errors = new ArrayList<String>();
+
+        String content_error = _validateContent(m.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+
+    // メッセージの必須入力チェック
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "メッセージを入力してください。";
+        }
+
+        return "";
+    }
+}
