af00527d1305156c35596ca022766fde6735bce7
yusuke.suzuki <mrs.aoringo@gmail.com>
2020-06-16 21:07:44 +0900

    vali task

--- src/models/validators/MessageValidator.java
+++ src/models/validators/MessageValidator.java
@@ -22,7 +22,7 @@ public class MessageValidator {
     // メッセージの必須入力チェック
     private static String _validateContent(String content) {
         if(content == null || content.equals("")) {
-            return "メッセージを入力してください。";
+            return "タスクを入力してください。";
         }
 
         return "";
